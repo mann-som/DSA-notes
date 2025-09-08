02a0906ddc95222696b8d02e7720d3c6cb34e842
Mann <mannsomofficial@gmail.com>
2025-09-08 18:15:44 +0530

    graph topological sort and undirected graph cycle

--- graph/shortestPathBFS.cpp
+++ graph/shortestPathBFS.cpp
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <list>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
 class Graph{
@@ -35,6 +36,37 @@ class Graph{
             }
 
             return false;
+        }
+
+        vector<int> shortestPath(int src, int dest){
+            queue<int> q;
+            unordered_map<int, int> parents;
+            unordered_map<int, bool> vis;
+
+            q.push(src);
+            vis[src] = 1;
+            parents[src] = -1;
+
+            while(!q.empty()){
+                int frontNode = q.front(); q.pop();
+
+                for(auto nbr : adjList[frontNode]){
+                    if(!vis[nbr]){
+                        q.push(nbr);
+                        parents[nbr] = src;
+                        vis[nbr] = 1;
+                    }
+                }
+            }
+
+            vector<int> ans;
+            while(dest != -1){
+                ans.push_back(dest);
+                dest = parents[dest];
+            }
+
+            reverse(ans.begin(), ans.end());
+            return ans;
         }
 };
 
--- graph/topologicalSort.cpp
+++ graph/topologicalSort.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include <unordered_map>
 #include <list>
+#include <stack>
 #include <queue>
 using namespace std;
 
@@ -35,6 +36,26 @@ class Graph{
             }
 
             return false;
+        }
+
+        void topologicalSort(int src, unordered_map<int, bool> vis, stack<int>& st){
+            vis[src] = 1;
+
+            for(auto child: adjList[src]){
+                if(!vis[child]) topologicalSort(child, vis, st);
+            }
+
+            st.push(src);
+        }
+
+        void topoSortBFS(){
+            queue<int> q;
+            unordered_map<int, int> indegrees;
+            for(auto i:adjList){
+                for(auto j:i.second){
+                    indegrees[j]++;
+                }
+            }
         }
 };
 
